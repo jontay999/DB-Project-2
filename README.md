a87f9494029a3f721f75b5a4f332f415404529ed
Jonathan Tay <jontay999@gmail.com>
2023-10-22 16:21:12 +0800

    add readme and pyqt

--- README.md
+++ README.md
@@ -51,8 +51,8 @@ brew services stop postgresql
 
 ```sql
 -- SET UP YOUR USER AND PASSWORD
-CREATE ROLE db_user WITH LOGIN PASSWORD 'db_password';
-ALTER ROLE db_user CREATEDB;
+CREATE ROLE postgres WITH LOGIN PASSWORD 'db_password';
+ALTER ROLE postgres CREATEDB;
 
 -- To view users and privileges, RUN THIS
 \du
@@ -66,3 +66,24 @@ user – '<your-db-user>'
 password – '<your-db-pw>'
 maintenance database – 'postgres'
 ```
+
+### Data Generation (MacOS)
+
+1. Follow setup instructions [here](https://github.com/gregrahn/tpch-kit/blob/master/README.md)
+2. `cd` into the `dbgen` folder and run `./dbgen` (make sure you specify your environment variable of output folder)
+3. (You don't have to remove trailing delimiters as this is done already)
+4. Run `ls | sed 's/^\(.*\)\.tbl$/mv "\1.tbl" "\1.csv"/' | sh` to rename all the `.tbl` to `.csv`
+
+With regard to actually loading in the data, I wanted to explore these various optimization techniques, but I think the most convenient is just to use `pgAdmin4`. If you want to try to script it, you can take a look at [https://hakibenita.com/fast-load-data-python-postgresql](https://hakibenita.com/fast-load-data-python-postgresql)
+
+### Things to take note of
+
+- `postgres` only allows LOWERCASE table names
+
+Handy cheatsheet of postgres commands
+
+- `\l` : show all databases
+- `\dt`: show all tables
+- `\du`: show all users
+- `\password`: change password
+- `psql postgres -U <user>`: login as specific user (bash)
--- src/db.py
+++ src/db.py
@@ -2,15 +2,24 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
+TABLE_FOLDER = "./sql_setup/"
+TABLE_FILES = ["region.sql", "nation.sql", "part.sql", "supplier.sql", "partsupp.sql", "customer.sql", "order.sql", "lineitem.sql"]
+DATABASE_NAME = "tpc_h"
+
+# for use before the TCP-H db exists
+STARTING_DATABASE_NAME = "postgres"
+
+
 class Database:
     # Maintain singleton design pattern with static variable
     _instance_exists = False
+    _created_tables = False
 
     def __init__(self):
         if self._instance_exists: raise Exception("Only one instance allowed!")
         self._instance_exists = True
         load_dotenv()
-        self.db_user = os.getenv('DB_USER', 'db_user')
+        self.db_user = os.getenv('DB_USER', 'postgres')
         self.db_password = os.getenv('DB_PASSWORD', 'db_password')
         self.db_host = os.getenv('DB_HOST', 'localhost')
         self.db_port = os.getenv('DB_PORT', 5432)
@@ -19,7 +28,7 @@ class Database:
     def is_connected(self):
         return self.connection != None
 
-    def connect(self, table_name):
+    def connect(self, table_name, read_only=True):
         if self.connection != None: raise Exception("Connection already exists!")
         connection = psycopg2.connect(
             database=table_name, 
@@ -28,21 +37,50 @@ class Database:
             host=self.db_host, 
             port=self.db_port
         )
-        connection.set_session(readonly=True, autocommit=True)
+        connection.set_session(readonly=read_only, autocommit=True)
         self.connection = connection
 
-    def execute(self, query):
+    def execute(self, query, needs_output=True):
         assert self.is_connected(), "Connection does not exist!"
-        cursor = self.connection.cursor()
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        cursor.close()
-        return rows
+        result = None
+        with self.connection.cursor() as cursor:
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+            if needs_output:
+                result = cursor.fetchall()
+        
+        if needs_output: return result
 
     def close(self):
         if self.connection == None: raise Exception("Connection does not exist!")
         self.connection.close()
+        self.connection = None
+
+
+    def create_tables(self, base_db_name, new_db_name):
+        self.connect(base_db_name, False)
+        self.execute(f"DROP DATABASE IF EXISTS {new_db_name};", False)
+        self.execute(f"CREATE DATABASE {new_db_name};", False)
+        self.execute("GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO user")
+        print(f"Created database: {new_db_name}")
+
+        self.close()
+
+        self.connect(new_db_name, False)
+        for file in TABLE_FILES:
+            with open(f"{TABLE_FOLDER}/{file}") as f:
+                cmd = f.read()
+                self.execute(cmd, False)
+            print(f"{file} has been created")
+        print("All tables created")
+        self.close()
+    
+    
 
 
 if __name__ == "__main__":
-    print("Running db.py! What do you think you're doing ....")
+    print("Running db.py! ONLY DO THIS WHEN YOU ARE CREATING TABLE")
+    db = Database()
+    db.create_tables(STARTING_DATABASE_NAME, DATABASE_NAME)
+
+
